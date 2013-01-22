b513e5c202c50b73d324c5a3aef3e9d0c892a4a1
William B. Wettersten <wbw20@case.edu>
2013-01-22 00:07:17 -0500

    serving static content

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+ 
+var http = require('http').createServer(function handler(req, res) {
+  fs.readFile(__dirname + '/views/index.html', function(err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+}).listen(8080);
